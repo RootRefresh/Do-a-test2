8fe79c886d3c6470c2fd8a402424351a92076991
RootRefresh <1779752566@qq.com>
2016-10-24 16:49:40 +0800

    init

---
+++ gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by USER on 16/10/24.
+//  Copyright © 2016年 xiacai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitDemo
+//
+//  Created by USER on 16/10/24.
+//  Copyright © 2016年 xiacai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
